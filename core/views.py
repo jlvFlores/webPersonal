13d25c5c64d16d2b18a6e7ccb30f1c128618839c
jluis <jluisvazquez.jv@gmail.com>
2022-06-03 16:52:53 -0500

    Páginas portafolio y contacto

--- core/views.py
+++ core/views.py
@@ -5,6 +5,7 @@ html_base = """
     <ul>
         <li><a href="/">Portada</a></li>
         <li><a href="/about/">Acerca de</a></li>
+        <li><a href="/contact/">Contacto</a></li>
     </ul>
 """
 
@@ -18,4 +19,15 @@ def about(request):
     return HttpResponse(html_base + """
         <h2>Acerca de</h2>
         <p>Me llamo Jose Luis y me encanta Django!</p>
+    """)
+
+def contact(request):
+    return HttpResponse(html_base + """
+        <h2>Contacto</h2>
+        <p>Aquí os dejo mi email y mis redes sociales:</p>
+        <ul>
+            <li><a href="mailto:hola@hektorprofe.net">Email</a></li>
+            <li><a href="https://github.com/hcosta">Github</a></li>
+            <li><a href="https://youtube.com">Youtube</a></li>
+        </ul>
     """)
