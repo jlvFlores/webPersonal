68b50349b9c2a1d772469c2a3dd18fa8e54876e3
jluis <jluisvazquez.jv@gmail.com>
2022-06-03 17:16:22 -0500

    Template tag {% url %}

--- webpersonal/urls.py
+++ webpersonal/urls.py
@@ -19,7 +19,7 @@ from core import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('about/', views.about, name="about"),
+    path('about-me/', views.about, name="about"),
     path('portfolio/', views.portfolio, name="portfolio"),
     path('contact/', views.contact, name="contact"),
     path('admin/', admin.site.urls),
